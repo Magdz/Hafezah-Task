f8f4918e1bbf4d2d0fd3b692bd15d6e3b07f1108
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2019-03-06 07:21:18 +0200

    Filter nearby restaurants

--- services/RestaurantService.py
+++ services/RestaurantService.py
@@ -1,3 +1,5 @@
+from math import sin, cos, sqrt, atan2, radians
+
 from ..repositories import RestaurantRepository
 
 class RestaurantService:
@@ -6,6 +8,32 @@ class RestaurantService:
 
     def list_nearby(self, data):
         repository = RestaurantRepository()
-        restaurants = repository.all()
+        all_restaurants = repository.all()
+        nearby_restaurants = self.__filter_nearby(data, all_restaurants)
 
-        return restaurants
+        return nearby_restaurants
+
+    def __filter_nearby(self, data, restaurants):
+        output = []
+        walkable_distance = 1
+
+        radius = 6373.0
+        person_lat = radians(data["latitude"])
+        person_lng = radians(data["longitude"])
+
+        for restaurant in restaurants:
+            restaurant_lat = radians(restaurant.latitude)
+            restaurant_lng = radians(restaurant.longitude)
+
+            lat_diff = restaurant_lat - person_lat
+            lng_diff = restaurant_lng - person_lng
+
+            a = sin(lat_diff / 2)**2 + cos(person_lat) * cos(restaurant_lat) * sin(lng_diff / 2)**2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+            distance = radius * c
+            if (distance <= walkable_distance):
+                output.append(restaurant)
+
+        return output
+        
